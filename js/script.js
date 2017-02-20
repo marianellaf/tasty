af4915db7dbbefaa927a47256fb8a8bb6a583b7c
javohotel <ja.ramirez@live.cl>
2017-02-20 17:15:20 -0400

    algunos cambios pequeños

--- js/script.js
+++ js/script.js
@@ -77,3 +77,57 @@ $(document).ready(function() {
   }
 
 });
+
+
+//efecto fotos
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  $(document).ready(function()
+    {
+      $('#waterfall').NewWaterfall();
+    });
+
+    // 自动加载更多
+    function random(min,max)
+    {
+      return min + Math.floor(Math.random() * (max - min + 1))
+    }
+    var loading  = false;
+    var dist = 300;
+    var num = 1;
+
+    setInterval(function(){
+      if($(window).scrollTop() >= $(document).height() - $(window).height() - dist && !loading)
+      {
+        loading = true;
+
+        $("#waterfall").append("<li><div style='height:" + random(50,500) +  "px'>" + num + "</div></li>");
+        num++;
+
+        loading = false;
+      }
+    },60);
+  </script>
+</body><script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
